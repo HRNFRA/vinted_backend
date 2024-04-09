ef62c070b699bcd86c34af09b76669e27b4bdaa5
HRNFRA <95613677+HRNFRA@users.noreply.github.com>
2024-04-09 14:20:56 +0200

    CORS

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(morgan("dev"));
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }))
 
